65924b0e6fd6c211b1aea8f555b0f61e70757af2
Varga Rego <varegab@gmail.com>
2021-02-13 16:11:12 +0100

    Modified               3.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM debian:stable-slim AS builder
+FROM archlinux:latest AS builder
 
 COPY install-packages.sh .
 RUN ./install-packages.sh
 
-FROM debian:stable-slim
-COPY --from=builder /usr/local/nagios /usr/local/nagios
-COPY --from=builder /usr/local/apache2 /usr/local/apache2
-COPY --from=builder /usr/lib/x86_64-linux-gnu /usr/lib/x86_64-linux-gnu
-COPY --from=builder /lib/x86_64-linux-gnu /lib/x86_64-linux-gnu
+#FROM archlinux:latest
+#COPY --from=builder /usr/local/nagios /usr/local/nagios
+#COPY --from=builder /usr/local/apache2 /usr/local/apache2
+#COPY --from=builder /usr/lib/x86_64-linux-gnu /usr/lib/x86_64-linux-gnu
+#COPY --from=builder /lib/x86_64-linux-gnu /lib/x86_64-linux-gnu
 # ENTRYPOINT ["/usr/local/nagios/bin/nagios", "/usr/local/nagios/etc/nagios.cfg"]
--- install-packages.sh
+++ install-packages.sh
@@ -6,34 +6,12 @@
 # details.
 set -euo pipefail
 
-# Tell apt-get we're never going to be able to give manual
-# feedback:
-export DEBIAN_FRONTEND=noninteractive
-
 APACHE="2.4.46"
 NAGIOS="4.4.6"
 
-# Update the package listing, so we know what package exist:
-apt-get update
 
-# Install security updates:
-apt-get -y upgrade
-
-# Install a new package, without unnecessary recommended packages:
-apt-get -y install --no-install-recommends autoconf gcc make libapr1-dev libaprutil1-dev libpcre3-dev wget unzip php ca-certificates
-
-# Delete cached files we don't need anymore:
-apt-get clean
-rm -rf /var/lib/apt/lists/*
-
-# Download Apache httpd from the official repo.
-cd /tmp
-wget https://downloads.apache.org//httpd/httpd-$APACHE.tar.gz
-tar -xvf httpd-$APACHE.tar.gz
-cd /tmp/httpd-$APACHE
-./configure --prefix=/usr/local/apache2 --enable-mods-shared=all
-make
-make install
+pacman --noconfirm -Syyu
+pacman --noconfirm -S gcc glibc make wget unzip apache php gd traceroute php-apache
 
 # Download and compile Nagios-core from the official repo.
 cd /tmp
@@ -41,12 +19,12 @@ wget https://assets.nagios.com/downloads/nagioscore/releases/nagios-$NAGIOS.tar.
 tar -xvf nagios-$NAGIOS.tar.gz
 cd /tmp/nagios-$NAGIOS
 mkdir /etc/apache2/sites-enabled
-./configure --with-httpd-conf=/etc/apache2/sites-enabled
+./configure --with-httpd-conf=/etc/httpd/conf/extra
 make all
 
 # This creates the nagios user and group. The www-data user is also added to the nagios group.
 make install-groups-users
-usermod -a -G nagios www-data
+usermod -a -G nagios http
 
 # This step installs the binary files, CGIs, and HTML files.
 make install
@@ -64,3 +42,17 @@ make install-config
 make install-webconf
 #a2enmod rewrite
 #a2enmod cgi
+
+sed -i 's/^#LoadModule mpm_prefork_module modules\/mod_mpm_prefork\.so/LoadModule mpm_prefork_module modules\/mod_mpm_prefork\.so/g' /etc/httpd/conf/httpd.conf
+sed -i 's/DirectoryIndex index.html/DirectoryIndex index.php index.html index.htm AddType application\/x-httpd-php .phpAddType application\/x-httpd-php-source .phps/g' /etc/httpd/conf/httpd.conf
+sed -i 's/#LoadModule cgid_module/LoadModule cgid_module/g' /etc/httpd/conf/httpd.conf
+sed -i 's/#LoadModule cgi_module/LoadModule cgi_module/g' /etc/httpd/conf/httpd.conf
+echo 'LoadModule php7_module modules/libphp7.so' >> /etc/httpd/conf/httpd.conf
+echo 'Include "conf/extra/nagios.conf"' >> /etc/httpd/conf/httpd.conf
+echo 'Include "conf/extra/php7_module.conf"' >> /etc/httpd/conf/httpd.conf
+printf '\n<FilesMatch ".php$">\n' >> /etc/httpd/conf/httpd.conf
+printf '\tSetHandler application/x-httpd-php\n' >> /etc/httpd/conf/httpd.conf
+printf '</FilesMatch>\n' >> /etc/httpd/conf/httpd.conf
+printf '<FilesMatch ".phps$">\n' >> /etc/httpd/conf/httpd.conf
+printf '\tSetHandler application/x-httpd-php-source\n' >> /etc/httpd/conf/httpd.conf
+printf '</FilesMatch>\n' >> /etc/httpd/conf/httpd.conf
