332ac16cd7e4ff02e3a915b9f375ed4c1322eb69
Varga Rego <varegab@gmail.com>
2021-02-14 11:25:41 +0100

    Starting with lighttpd            .

--- install-packages.sh
+++ install-packages.sh
@@ -10,7 +10,6 @@ set -euo pipefail
 # feedback:
 export DEBIAN_FRONTEND=noninteractive
 
-APACHE="2.4.46"
 NAGIOS="4.4.6"
 
 # Update the package listing, so we know what package exist:
@@ -20,47 +19,32 @@ apt-get update
 apt-get -y upgrade
 
 # Install a new package, without unnecessary recommended packages:
-apt-get -y install --no-install-recommends autoconf gcc make libapr1-dev libaprutil1-dev libpcre3-dev wget unzip php ca-certificates
+apt-get -y install --no-install-recommends lighttpd autoconf gcc libc6 make wget ca-certificates unzip php libgd-dev
 
 # Delete cached files we don't need anymore:
 apt-get clean
 rm -rf /var/lib/apt/lists/*
-
-# Download Apache httpd from the official repo.
-cd /tmp
-wget https://downloads.apache.org//httpd/httpd-$APACHE.tar.gz
-tar -xvf httpd-$APACHE.tar.gz
-cd /tmp/httpd-$APACHE
-./configure --prefix=/usr/local/apache2 --enable-mods-shared=all
-make
-make install
 
 # Download and compile Nagios-core from the official repo.
 cd /tmp
 wget https://assets.nagios.com/downloads/nagioscore/releases/nagios-$NAGIOS.tar.gz
 tar -xvf nagios-$NAGIOS.tar.gz
 cd /tmp/nagios-$NAGIOS
-mkdir /etc/apache2/sites-enabled
-./configure --with-httpd-conf=/etc/apache2/sites-enabled
+./configure
 make all
 
 # This creates the nagios user and group. The www-data user is also added to the nagios group.
 make install-groups-users
-usermod -a -G nagios www-data
+#usermod -a -G nagios www-data
 
 # This step installs the binary files, CGIs, and HTML files.
 make install
 
 # This installs the service or daemon files and also configures them to start on boot.
-make install-daemoninit
+make install-init
 
 # This installs and configures the external command file.
 make install-commandmode
 
 # This installs the *SAMPLE* configuration files. These are required as Nagios needs some configuration files to allow it to start.
 make install-config
-
-# This installs the Apache web server configuration files and configures the Apache settings.
-make install-webconf
-#a2enmod rewrite
-#a2enmod cgi
