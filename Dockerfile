4e90eece563536ed9ceddc01836bdc90dbbbc19b
Varga Rego <varegab@gmail.com>
2021-02-15 01:39:40 +0100

    commit                         .

--- Dockerfile
+++ Dockerfile
@@ -4,39 +4,7 @@ COPY install-packages.sh .
 RUN ./install-packages.sh
 
 # Multi-stage build
-FROM debian:stable-slim
+#FROM debian:stable-slim
 
-# Copy Apache
-COPY --from=builder /usr/lib/apache2 /usr/lib/apache2
-COPY --from=builder /usr/lib/php /usr/lib/php
-COPY --from=builder /usr/lib/x86_64-linux-gnu/libaprutil-1.so.0 /usr/lib/x86_64-linux-gnu/libaprutil-1.so.0
-COPY --from=builder /usr/lib/x86_64-linux-gnu/libapr-1.so.0 /usr/lib/x86_64-linux-gnu/libapr-1.so.0
-COPY --from=builder /usr/lib/x86_64-linux-gnu/libargon2.so.1 /usr/lib/x86_64-linux-gnu/libargon2.so.1
-COPY --from=builder /usr/lib/x86_64-linux-gnu/libxml2.so.2 /usr/lib/x86_64-linux-gnu/libxml2.so.2
-COPY --from=builder /usr/lib/x86_64-linux-gnu/libssl.so.1.1 /usr/lib/x86_64-linux-gnu/libssl.so.1.1
-COPY --from=builder /usr/lib/x86_64-linux-gnu/libcrypto.so.1.1 /usr/lib/x86_64-linux-gnu/libcrypto.so.1.1
-COPY --from=builder /usr/lib/x86_64-linux-gnu/libpcre2-8.so.0 /usr/lib/x86_64-linux-gnu/libpcre2-8.so.0
-COPY --from=builder /usr/lib/x86_64-linux-gnu/libsodium.so.23 /usr/lib/x86_64-linux-gnu/libsodium.so.23
-COPY --from=builder /usr/lib/x86_64-linux-gnu/libicui18n.so.63 /usr/lib/x86_64-linux-gnu/libicui18n.so.63
-COPY --from=builder /usr/lib/x86_64-linux-gnu/libicuuc.so.63 /usr/lib/x86_64-linux-gnu/libicuuc.so.63
-COPY --from=builder /usr/lib/x86_64-linux-gnu/libicudata.so.63 /usr/lib/x86_64-linux-gnu/libicudata.so.63
-COPY --from=builder /lib/lsb /lib/lsb
-COPY --from=builder /lib/x86_64-linux-gnu /lib/x86_64-linux-gnu
-COPY --from=builder /var/lib/apache2 /var/lib/apache2
-COPY --from=builder /var/www /var/www
-COPY --from=builder /var/log/apache2 /var/log/apache2
-COPY --from=builder /usr/sbin /usr/sbin
-
-# Copy Nagios
-COPY --from=builder /usr/local/nagios /usr/local/nagios
-
-# Copy system etc
-COPY --from=builder /etc /etc
-
-# Copy awk, need for plugins
-COPY --from=builder /usr/bin/awk /usr/bin/awk
-COPY --from=builder /usr/lib/x86_64-linux-gnu/libsigsegv.so.2 /usr/lib/x86_64-linux-gnu/libsigsegv.so.2
-COPY --from=builder /usr/lib/x86_64-linux-gnu/libmpfr.so.6 /usr/lib/x86_64-linux-gnu/libmpfr.so.6
-
-COPY start.sh /start.sh
-CMD /start.sh
+#COPY start.sh /start.sh
+#CMD /start.sh
--- install-packages.sh
+++ install-packages.sh
@@ -10,9 +10,6 @@ set -euo pipefail
 # feedback:
 export DEBIAN_FRONTEND=noninteractive
 
-NAGIOS="4.4.6"
-PLUGINS="2.3.3"
-
 # Update the package listing, so we know what package exist:
 apt-get update
 
@@ -20,54 +17,16 @@ apt-get update
 apt-get -y upgrade
 
 # Install new packages for nagios install
-apt-get install -y autoconf gcc libc6 make wget unzip apache2 apache2-utils php libgd-dev ca-certificates
+apt-get install -y apt-get install wget apt-transport-https ca-certificates
 
-# Install new packages for plugins install
-apt-get install -y libmcrypt-dev bc gawk dc build-essential snmp libnet-snmp-perl gettext
+echo "deb https://repo.nagios.com/deb/$(lsb_release -cs) /" > /etc/apt/sources.list.d/nagios.list
+
+# Add our public GPG key
+wget -qO - https://repo.nagios.com/GPG-KEY-NAGIOS-V2 | apt-key add -
+
+# Update your repositories
+apt-get update
 
 # Delete cached files we don't need anymore:
 apt-get clean
 rm -rf /var/lib/apt/lists/*
-
-
-# Download and compile Nagios-core from the official repo.
-cd /tmp
-wget https://assets.nagios.com/downloads/nagioscore/releases/nagios-$NAGIOS.tar.gz
-tar -xvf nagios-$NAGIOS.tar.gz
-cd ./nagios-$NAGIOS
-./configure --with-httpd-conf="/etc/apache2/sites-enabled"
-make all
-
-# This creates the nagios user and group. The www-data user is also added to the nagios group.
-make install-groups-users
-usermod -a -G nagios www-data
-
-# This step installs the binary files, CGIs, and HTML files.
-make install
-
-# This installs the service or daemon files and also configures them to start on boot.
-make install-init
-
-# This installs and configures the external command file.
-make install-commandmode
-
-# This installs the *SAMPLE* configuration files. These are required as Nagios needs some configuration files to allow it to start.
-make install-config
-
-# This installs the Apache web server configuration files and configures the Apache settings.
-make install-webconf
-a2enmod rewrite
-a2enmod cgi
-
-# Create nagiosadmin user with default password "nagios".
-htpasswd -b -c /usr/local/nagios/etc/htpasswd.users nagiosadmin nagios
-
-# Install plugins
-cd /tmp
-wget https://github.com/nagios-plugins/nagios-plugins/releases/download/release-$PLUGINS/nagios-plugins-$PLUGINS.tar.gz
-tar -xvf nagios-plugins-$PLUGINS.tar.gz
-cd ./nagios-plugins-$PLUGINS
-#./tools/setup
-./configure
-make
-make install
